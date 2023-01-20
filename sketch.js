6784bac3292018175241009eeeacf926a84b23a7
Ryan Riley Puzon <rrgpuzon@gmail.com>
2023-01-19 18:35:04 -0800

    wavey squares

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+let angle = 0;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  translate(width/2, height/2);
+  rectMode(CENTER);
+
+
+  let offset = 0;
+  for(let x = 0; x < width; x+=10) {
+    let a = angle + offset;
+    let h = map(sin(a), -1, 1, 0, 100);
+    fill(255);
+    rect(x - width / 2,0,40,h);
+    offset += 0.25;
+  }
+
+  angle += 0.05;
 }
